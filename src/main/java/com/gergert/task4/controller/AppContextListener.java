01fe74299c34ae643a2bfd056d90c3e45449b7ef
IlyaGergert <IlyaGergert@gmail.com>
2026-01-31 18:23:48 +0300

    Add a new AppContextListener class with contextInitialized and contextDestroyed methods for creating a pool and destroying a pool

--- src/main/java/com/gergert/task4/controller/AppContextListener.java
+++ src/main/java/com/gergert/task4/controller/AppContextListener.java
@@ -1,4 +1,20 @@
 package com.gergert.task4.controller;
 
-public class AppContextListener {
+import com.gergert.task4.model.pool.ConnectionPool;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ConnectionPool.getInstance();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ConnectionPool.getInstance().destroyPool();
+    }
 }
